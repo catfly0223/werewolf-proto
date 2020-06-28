885acd7182c034a4206661557303f501d82699ac
Itsuki Yashina <iyashina0223@gmail.com>
2020-06-28 12:47:40 +0900

    Add players and roles (proto)

---
+++ src/components/Game.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="game">
+    <AddPlayer v-on:add-player="addPlayer" />
+    <Board :players="players"/>
+    <button v-on:click="assignRoles">Assign Roles</button>
+  </div>
+</template>
+
+<script>
+import Board from "./Board";
+import AddPlayer from "./AddPlayer";
+
+export default {
+  name: 'Game',
+  components:{
+    Board,
+    AddPlayer
+  },
+  data() {
+    return {
+      players: [
+        {
+          id: "1",
+          name: "Taro",
+          role: "人狼"
+        },
+        {
+          id: "2",
+          name: "Jiro",
+          role: "市民"
+        },
+        {
+          id: "3",
+          name: "Saburo",
+          role: "騎士"
+        }
+      ]
+    }
+  },
+    methods: {
+      addPlayer(newPlayer) {
+        this.players = [...this.players, newPlayer]
+      },
+      assignRoles: function() {
+        this.players.forEach(player =>
+          player.role = getRandomRole()
+        )
+  },
+    }
+}
+
+function getRandomRole() {
+  var roles = [
+    { value: "Citizen", text: "市民" },
+    { value: "Warewolf", text: "人狼" },
+    { value: "FortuneTeller", text: "占い師" },
+  ]
+  var index = Math.floor(Math.random() * roles.length)
+
+  return roles[index].text;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
---
+++ src/components/Player.vue
@@ -0,0 +1,23 @@
+<template>
+  <button>
+        {{ name }}
+        {{ role }}
+        {{ status }}
+  </button>
+</template>
+
+<script>
+export default {
+    name: "Player",
+    props: {
+        id: String,
+        name: String,
+        role: String,
+        status: String,
+    }
+}
+</script>
+
+<style>
+
+</style>
